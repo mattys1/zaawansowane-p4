f3b6061e7421bf2b6fc103fa61db9fd4e8376163
mattys1 <mattys0082@gmail.com>
2024-12-20 12:55:20 +0100

    doxygen

--- docs/html/search/all_7.js
+++ docs/html/search/all_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['wiersz_0',['wiersz',['../classMatrix.html#ab2f554376fb559a05d2b4789e0b0bc75',1,'Matrix']]],
-  ['wypisz_1',['wypisz',['../classMatrix.html#a25959aacfe474559046fbad8363c2fd9',1,'Matrix']]]
+  ['pod_5fprzekatna_0',['pod_przekatna',['../classMatrix.html#a794292f429843b21eaf1c85ad49ea660',1,'Matrix']]],
+  ['pokaz_1',['pokaz',['../classMatrix.html#afebea0d35ed9f69d221567dd3409bc3b',1,'Matrix']]],
+  ['przekatna_2',['przekatna',['../classMatrix.html#aaa16e344e5f7e2afd97857f517039780',1,'Matrix']]]
 ];
